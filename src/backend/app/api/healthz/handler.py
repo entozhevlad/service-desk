8f796ee9de6d5734420695565d5c92aa162a91fa
entozhevlad <vladislavzarubin113@gmail.com>
2026-02-24 23:38:36 +0300

    Инициализация бэкенд-сервиса

---
+++ src/backend/app/api/healthz/handler.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+from app.api.healthz.schemas import HealthResponse
+
+health_router = APIRouter(tags=["healthz"])
+
+
+@health_router.get("/healthz", response_model=HealthResponse)
+async def health_check() -> HealthResponse:
+    return HealthResponse(
+        status="ok",
+    )
---
+++ src/backend/app/routers.py
@@ -0,0 +1,3 @@
+from app.api.healthz.handler import health_router
+
+all_routers = [health_router]
